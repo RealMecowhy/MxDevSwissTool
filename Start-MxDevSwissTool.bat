a963fb67f676c94f117d709eb11bfdde3e6580d7
Mikolaj Jankiewicz <mikolaj.jankiewicz@raben-group.com>
2026-07-14 23:33:59 +0200

    chore: release v1.4.3

--- Start-MxDevSwissTool.bat
+++ Start-MxDevSwissTool.bat
@@ -4,16 +4,16 @@ echo ==============================================================
 echo             MxDev Swiss Tool Launcher
 echo ==============================================================
 echo.
-echo Zamykanie starych procesow Agenta...
+echo Closing old Agent processes...
 for /f "tokens=5" %%a in ('netstat -aon ^| find "9999" ^| find "LISTENING"') do taskkill /f /pid %%a >nul 2>&1
 echo.
-echo Uruchamianie Mendix Observability Bridge...
+echo Starting Mendix Observability Bridge...
 start "Mendix Observability Agent" cmd /k "node server/mendix-observability-bridge.js"
 echo.
-echo Otwieranie interfejsu MxDev Swiss Tool w domyslnej przegladarce...
+echo Opening MxDev Swiss Tool interface in default browser...
 start http://localhost:9999/
 echo.
-echo Gotowe! Mozesz juz zamknac to okienko startowe.
-echo [!] PAMIETAJ: Nie zamykaj drugiego czarnego okna "Mendix Observability Agent", dopoki korzystasz z narzedzia!
+echo Done! You can close this launcher window now.
+echo [!] REMEMBER: Do not close the second black window "Mendix Observability Agent" while using the tool!
 echo.
 timeout /t 5 >nul
